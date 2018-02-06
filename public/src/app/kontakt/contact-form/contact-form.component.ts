a7b2f0f42a14c8725057b755baf8e1919a57041e
aleksgrunwald <aleks.grunwald@gmail.com>
2018-02-06 19:28:06 +0100

    emailRegEx is nice but space before is allowed.

--- public/src/app/kontakt/contact-form/contact-form.component.ts
+++ public/src/app/kontakt/contact-form/contact-form.component.ts
@@ -14,9 +14,24 @@ export class ContactFormComponent {
   constructor(private messageService: MessageService) {
   }
 
+  emailRegEx: RegExp
+  emailIsInvalid: boolean
 
   phoneRegEx: RegExp
   phoneIsInvalid: boolean
+
+
+  validateEmail(emailInput) {
+    this.emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
+    if (!this.emailRegEx.test(emailInput.value)) {
+      console.log('emailIsInvalid - regex nie zgodny ' + this.emailIsInvalid)
+      return this.emailIsInvalid = true
+    } else {
+      console.log('emailIsInvalid - regex ZGODNY ' + this.emailIsInvalid)
+      return this.emailIsInvalid = false
+    }
+  }
+
 
   validatePhone(phoneInput) {
     this.phoneRegEx = /^(\+)*(\d(\ )*(-)*)+$/g
